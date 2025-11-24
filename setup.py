5176bb472c997908912564c0ee288fcb48320d4a
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2025-11-24 18:08:14 +0500

    chore: change package name to edx-games to publish it to pypi

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def package_data(pkg, roots):
 
 
 setup(
-    name="games",
+    name="edx-games",
     version="1.0.0",
     description="Interactive games XBlock for Open edX - Create flashcards and matching games with image support",
     author="edX",
