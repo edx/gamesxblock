652d33ea07623796e54774462ef9c9ac363734dc
Pradeep Patro <ppatro-apphelix@2u.com>
2025-11-29 02:31:41 +0530

    feat: Bump version to 1.0.4 in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def package_data(pkg, roots):
 
 setup(
     name="edx-games",
-    version="1.0.3",
+    version="1.0.4",
     description="Interactive games XBlock for Open edX - Create flashcards and matching games with image support",
     author="edX",
     author_email="edx@edx.org",
