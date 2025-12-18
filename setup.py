3082e59e2095d4863b865b3cb5fd28e57ecdfa68
Vivek Ambaliya <vjagdishbhaiambaliya-apphelix@2u.com>
2025-12-18 14:24:14 +0000

    chore: bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def package_data(pkg, roots):
 
 setup(
     name="edx-games",
-    version="1.0.6",
+    version="1.0.7",
     description="Interactive games XBlock for Open edX - Create flashcards and matching games with image support",
     author="edX",
     author_email="edx@edx.org",
